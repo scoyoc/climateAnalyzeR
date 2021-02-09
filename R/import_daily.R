9b902faf4ce7a1e525c864884e39d6753b08b500
scoyoc <scoyoc@gmail.com>
2021-02-08 17:34:35 -0700

    Standartized variable names and added an option to convert data from standard to metric.

--- R/import_daily.R
+++ R/import_daily.R
@@ -3,18 +3,31 @@
 #' @param station The character string of the station name.
 #' @param start_year The four digit number of the first year of interest.
 #' @param end_year The four digit number of the last year of interest.
+#' @param convert Logical. If TRUE, data are precipitation and temperature
+#'     values are converted to metric. These converted values are included as
+#'     additional columns in the data frame denoted by "_mm" or "_C". Default is
+#'     FALSE.
 #'
 #' @return A \code{\link[tibble:tibble]{tibble}}.
 #'
 #' @export
 #'
 #' @examples
-#' import_daily("arches", 2010, 2020)
+#' import_daily("hans_flat_rs", 2010, 2020)
 #'
-import_daily <- function(station, start_year, end_year){
+import_daily <- function(station, start_year, end_year, convert = FALSE){
   my_url = paste0("http://climateanalyzer.science/python/u_thresh.py?station=",
                   station, "&year1=", start_year, "&year2=", end_year, "&title=",
                   station, "&lowerthresh=daily&upperthresh=70&station_type=GHCN&csv=true&param=temperature&time_mode=year&first_month=01&last_month=12&ann_sum=False&screen_blanks=False")
   dat = pull_csv(my_url, skip = 2)
+  dat = rename_vars(dat)
+
+  # Convert to metric
+  if (convert == TRUE){
+    dat = convert_prcp(dat)
+    dat = convert_temp(dat)
+  }
+
+
   return(tibble::as_tibble(dat))
 }
--- R/import_monthly.R
+++ R/import_monthly.R
@@ -11,10 +11,22 @@
 #' @return A \code{\link[tibble]{tibble}}.
 #'
 #' @examples
-#' import_monthly("arches", 2000, 2020, month = "all", table_type = "straight",
-#'                norm_per = 'null')
-#' import_montly_wx('arches', 2000, 2010)
-#' import_monthly_depart('arches', 2000, 2010)
+#' # Import monthly departures
+#' import_monthly("natural_bridges_nm", 2000, 2020, month = "all",
+#'                table_type = "straight", norm_per = '1981-2010')
+#' # Import monthly precipitation and temperature data
+#' import_monthly("canyonlands_theneedle", 2000, 2020, month = "all",
+#'                table_type = "30dep", norm_per = 'null')
+#'
+#' # Import monthly departures using the import_monthly_depart() wrapper.
+#' import_monthly_depart('natural_bridges_nm', 2000, 2010)
+#' import_monthly_depart('natural_bridges_nm', 2000, 2010, convert = TRUE)
+#'
+#' # Import monthly precipitation and temperature data using the
+#' # import_montly_wx() wrapper.
+#' import_montly_wx('canyonlands_theneedle', 2000, 2010, convert = TRUE)
+#' # Import weather data for June
+#' import_montly_wx('hovenweep_utah', 2000, 2010, month = 6)
 #' @export import_monthly
 import_monthly <- function(station, start_year, end_year, month, table_type,
                            norm_per){
@@ -34,9 +46,23 @@ import_monthly <- function(station, start_year, end_year, month, table_type,
 #' @param end_year The four digit number of the last year of interest.
 #' @param month The month number (i.e., 1 for January through 12 for December)
 #'     or 'all' for all months.
-import_montly_wx <- function(station, start_year, end_year, month = 'all'){
-  dat = import_monthly(station, start_year, end_year, month,
-                          table_type = "straight", norm_per = 'null')
+#' @param norm_per A character string for the 30-year normalization period.
+#' @param convert Logical. If TRUE, data are precipitation and temperature
+#'     values are converted to metric. These converted values are included as
+#'     additional columns in the data frame denoted by "_mm" or "_C". Default is
+#'     FALSE.
+import_monthly_depart <- function(station, start_year, end_year,
+                                  month = 'all', norm_per = '1981-2010',
+                                  convert = FALSE){
+  dat = import_monthly(station, start_year, end_year, month = month,
+                 table_type = "30dep", norm_per = norm_per)
+  colnames(dat) = c("Year", "Month", "PRCP_pctavg", "TMAX_depart", "TMIN_depart")
+
+  # Convert to metric
+  if (convert == TRUE){
+    dat = convert_temp(dat)
+  }
+
   return(tibble::as_tibble(dat))
 }
 
@@ -47,10 +73,21 @@ import_montly_wx <- function(station, start_year, end_year, month = 'all'){
 #' @param end_year The four digit number of the last year of interest.
 #' @param month The month number (i.e., 1 for January through 12 for December)
 #'     or 'all' for all months.
-#' @param norm_per A character string for the 30-year normalization period.
-import_monthly_depart <- function(station, start_year, end_year,
-                                     month = 'all', norm_per = '1981-2010'){
-  dat = import_monthly(station, start_year, end_year, month = month,
-                 table_type = "30dep", norm_per = norm_per)
+#' @param convert Logical. If TRUE, data are precipitation and temperature
+#'     values are converted to metric. These converted values are included as
+#'     additional columns in the data frame denoted by "_mm" or "_C". Default is
+#'     FALSE.
+import_montly_wx <- function(station, start_year, end_year, month = 'all',
+                             convert = FALSE){
+  dat = import_monthly(station, start_year, end_year, month,
+                       table_type = "straight", norm_per = 'null')
+  colnames(dat) = c("Year", "Month", "PRCP", "TMAX", "TMIN")
+
+  # Convert to metric
+  if (convert == TRUE){
+    dat = convert_prcp(dat)
+    dat = convert_temp(dat)
+  }
+
   return(tibble::as_tibble(dat))
 }
